c0b085b3d05789a0171a5a5952b29c23ece9ce6b
root <root@srv599215.hstgr.cloud>
2025-03-15 14:54:16 +0800

    f

--- upward-api/src/controller/Reference/Id-Entry.ts
+++ upward-api/src/controller/Reference/Id-Entry.ts
@@ -784,7 +784,6 @@ FROM
       });
     },
     onDraw: (props: any, rowItm: any, rowIdx: number) => {
-  
     },
     afterDraw: (props: any, worksheet: any) => {
       // props.boldText(1, ["A", "B", "C", "D", "E", "F", "G", "H"]);
