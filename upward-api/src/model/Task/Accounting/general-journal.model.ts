c195b476fa9f276f15f9f69a6a3c193218dad60c
root <root@srv599215.hstgr.cloud>
2025-03-21 15:16:45 +0800

    f

--- upward-api/src/model/Task/Accounting/general-journal.model.ts
+++ upward-api/src/model/Task/Accounting/general-journal.model.ts
@@ -304,7 +304,7 @@ export async function getSelectedSearchGeneralJournal(
         OR_Invoice_No as invoice,
         a.VATItemNo AS TempID
     FROM
-      journal_voucher a where a.Source_No ='${Source_No}' order by a.GL_Acct
+      journal_voucher a where a.Source_No ='${Source_No}' order by a.GL_Acct asc
       `);
 }
 
