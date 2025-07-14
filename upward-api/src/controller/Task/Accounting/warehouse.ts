166161e82a16686d96ad07478bf5364ad28ae6fb
charrlesss <charlespalencia21@gmail.com>
2025-07-14 16:30:18 +0800

    f

--- upward-api/src/controller/Task/Accounting/warehouse.ts
+++ upward-api/src/controller/Task/Accounting/warehouse.ts
@@ -16,38 +16,7 @@ import { prisma } from "../..";
 
 const Warehouse = express.Router();
 
-Warehouse.post("/warehouse/load-list", async (req, res) => {
-  try {
-    res.send({
-      message: `Successfully Get Data`,
-      success: true,
-      list: await loadList(req, req.body.RCPNo),
-    });
-  } catch (err: any) {
-    console.log(err.message);
-    res.send({
-      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
-      success: false,
-      data: [],
-    });
-  }
-});
-Warehouse.post("/warehouse/get-pullout-rcpno", async (req, res) => {
-  try {
-    res.send({
-      message: `Successfully Get Data`,
-      success: true,
-      data: await getApprovedRCPNo(req),
-    });
-  } catch (err: any) {
-    console.log(err.message);
-    res.send({
-      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
-      success: false,
-      data: [],
-    });
-  }
-});
+
 Warehouse.post(
   "/warehouse/search-pdc-checks-client-policy",
   async (req, res) => {
@@ -269,184 +238,43 @@ Warehouse.post("/warehouse/report", async (req, res) => {
 
 ///////////////////////////// NEW /////////////////////
 
-Warehouse.post("/warehouse/search-by-policy", async (req, res) => {
-  const StrQry = `
-     SELECT 
-          DATE_FORMAT(policy.DateIssued, '%M. %d, %Y') AS Date,
-          policy.PolicyNo,
-          policy.Account,
-          ID_Entry.Shortname AS Name
-      FROM
-          policy
-              LEFT JOIN
-          fpolicy ON policy.PolicyNo = fpolicy.PolicyNo
-              LEFT JOIN
-          vpolicy ON policy.PolicyNo = vpolicy.PolicyNo
-              LEFT JOIN
-          mpolicy ON policy.PolicyNo = mpolicy.PolicyNo
-              LEFT JOIN
-          bpolicy ON policy.PolicyNo = bpolicy.PolicyNo
-              LEFT JOIN
-          msprpolicy ON policy.PolicyNo = msprpolicy.PolicyNo
-              LEFT JOIN
-          papolicy ON policy.PolicyNo = papolicy.PolicyNo
-              LEFT JOIN
-          cglpolicy ON policy.PolicyNo = cglpolicy.PolicyNo
-              LEFT JOIN
-          (SELECT 
-              id_entry.IDNo, id_entry.ShortName AS Shortname, IDType
-          FROM
-              (SELECT 
-              IF(aa.option = 'individual', CONCAT(IF(aa.lastname IS NOT NULL
-                      AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname, ' ', aa.suffix, '.'), aa.company) AS ShortName,
-                  aa.entry_client_id AS IDNo,
-                  aa.sub_account,
-                  'Client' AS IDType
-          FROM
-              entry_client aa UNION ALL SELECT 
-              CONCAT(IF(aa.lastname IS NOT NULL
-                      AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname) AS ShortName,
-                  aa.entry_agent_id AS IDNo,
-                  aa.sub_account,
-                  'Agent' AS IDType
-          FROM
-              entry_agent aa UNION ALL SELECT 
-              CONCAT(IF(aa.lastname IS NOT NULL
-                      AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname) AS ShortName,
-                  aa.entry_employee_id AS IDNo,
-                  aa.sub_account,
-                  'Employee' AS IDType
-          FROM
-              entry_employee aa UNION ALL SELECT 
-              aa.fullname AS ShortName,
-                  aa.entry_fixed_assets_id AS IDNo,
-                  sub_account,
-                  'Fixed Assets' AS IDType
-          FROM
-              entry_fixed_assets aa UNION ALL SELECT 
-              aa.description AS ShortName,
-                  aa.entry_others_id AS IDNo,
-                  aa.sub_account,
-                  'Others' AS IDType
-          FROM
-              entry_others aa UNION ALL SELECT 
-              IF(aa.option = 'individual', CONCAT(IF(aa.lastname IS NOT NULL
-                      AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname), aa.company) AS ShortName,
-                  aa.entry_supplier_id AS IDNo,
-                  aa.sub_account,
-                  'Supplier' AS IDType
-          FROM
-              entry_supplier aa) id_entry)  AS ID_Entry ON policy.IDNo = ID_Entry.IDNo
-      WHERE
-          policy.PolicyNo IN (SELECT 
-                  PNo
-              FROM
-                  pdc)
-              AND ((vpolicy.ChassisNo LIKE ?)
-              OR (vpolicy.MotorNo LIKE ?)
-              OR (vpolicy.PlateNo LIKE ?)
-              OR (ID_Entry.Shortname LIKE ?)
-              OR (vpolicy.PolicyNo LIKE ?)
-              OR (vpolicy.Account LIKE ?))
-      ORDER BY policy.DateIssued DESC
-      LIMIT 500
-        `;
+Warehouse.post("/warehouse/search-pdc", async (req, res) => {
   try {
-    res.send({
-      message: "successfully",
-      success: true,
-      data: await prisma.$queryRawUnsafe(
-        StrQry,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`
-      ),
-    });
-  } catch (err: any) {
-    console.log(err.message);
-    res.send({
-      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
-      success: false,
-      data: [],
-    });
-  }
-});
-Warehouse.post("/warehouse/search-by-client-id", async (req, res) => {
-  try {
-    const StrQry = `
-      select * from (  SELECT  
-        IDNo AS IDNo, 
-        Shortname AS Name, 
-        IDType 
-      FROM (SELECT 
-       id_entry.IDNo,
-       id_entry.ShortName as Shortname,
-       IDType
-   FROM
-       (SELECT 
-           IF(aa.option = 'individual', 
-           CONCAT(IF(aa.lastname IS NOT NULL AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname ,' ',aa.suffix,'.'), aa.company) AS ShortName,
-               aa.entry_client_id AS IDNo,
-               aa.sub_account,
-               'Client' as IDType
-       FROM
-           entry_client aa 
-           UNION ALL SELECT 
-           CONCAT(IF(aa.lastname IS NOT NULL
-                   AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname) AS ShortName,
-               aa.entry_agent_id AS IDNo,
-               aa.sub_account,
-               'Agent' as IDType
-       FROM
-           entry_agent aa 
-           UNION ALL SELECT 
-           CONCAT(IF(aa.lastname IS NOT NULL
-                   AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname) AS ShortName,
-               aa.entry_employee_id AS IDNo,
-               aa.sub_account,
-               'Employee' as IDType
-       FROM
-           entry_employee aa 
-           UNION ALL SELECT 
-           aa.fullname AS ShortName,
-               aa.entry_fixed_assets_id AS IDNo,
-               sub_account,
-                'Fixed Assets' as IDType
-       FROM
-           entry_fixed_assets aa 
-           UNION ALL SELECT 
-           aa.description AS ShortName,
-               aa.entry_others_id AS IDNo,
-               aa.sub_account,
-               'Others' as IDType
-       FROM
-           entry_others aa 
-           UNION ALL SELECT 
-           IF(aa.option = 'individual', CONCAT(IF(aa.lastname IS NOT NULL
-                   AND TRIM(aa.lastname) <> '', CONCAT(aa.lastname, ', '), ''), aa.firstname), aa.company) AS ShortName,
-               aa.entry_supplier_id AS IDNo,
-               aa.sub_account,
-                'Supplier' as IDType
-       FROM
-           entry_supplier aa) id_entry) as ID_Entry
-      WHERE 
-      ID_Entry.IDNo in (select PNo from pdc)) IDs
-      where 
-      IDs.IDNo LIKE ? OR  
-      IDs.Name LIKE ?
-      ORDER BY IDs.Name
-      limit 500;`;
+    let strWhere = "";
+    const statusOptions = ["Received", "Stored", "Stored"];
+    const selectedIndex = req.body.pdcStatus;
+    let status = "";
+    if (selectedIndex >= 0) {
+      status = statusOptions[selectedIndex];
+      if (selectedIndex !== 2) {
+        strWhere = `(PDC_Status = ?)`;
+      } else {
+        strWhere = `(PDC_Status = ? OR (PDC_Status = 'Pulled Out' AND (PDC_Remarks = 'Fully Paid' OR PDC_Remarks = 'Replaced')))`;
+      }
+    }
+
+    const qry = `
+      SELECT 
+        PNo, 
+        IDNo, 
+        DATE_FORMAT(Date,'%m/%d/%Y') AS Date, 
+        Name, 
+        date_format(Check_Date,'%m/%d/%Y') AS CheckDate, 
+        Check_No, 
+        Check_Amnt, 
+        Bank, 
+        PDC_Status
+      FROM pdc  
+      WHERE  (PNo = ? OR  IDNo = ?) AND ${strWhere} ORDER BY Check_Date`;
 
     res.send({
       message: "successfully",
       success: true,
       data: await prisma.$queryRawUnsafe(
-        StrQry,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`
+        qry,
+        req.body.search,
+        req.body.search,
+        status
       ),
     });
   } catch (err: any) {
@@ -458,30 +286,99 @@ Warehouse.post("/warehouse/search-by-client-id", async (req, res) => {
     });
   }
 });
-Warehouse.post("/warehouse/search-by-bank", async (req, res) => {
+Warehouse.post("/warehouse/save-checks", async (req, res) => {
   try {
-    const StrQry = `
-      SELECT 
-        Bank_Code AS Code,
-        Bank AS Bank_Name
-       FROM Bank 
-       WHERE 
-       Inactive = 0 AND 
-       Bank_Code in (SELECT Bank FROM pdc group by Bank) and
-       (
-       Bank_Code LIKE ?  OR  
-       Bank LIKE ? ) 
-       ORDER BY Bank
-       limit 500 
-       `;
+
+    const pdcStatusIndex = parseInt(req.body.pdcStatus.toString());
+    const remarks = req.body.remarksValue;
+    const tableDataSelected = req.body.tableData;
+
+    if (pdcStatusIndex === 2) {
+      for (const itm of tableDataSelected) {
+        const qry = `
+                  select 
+                    *
+                  from pullout_request as POR 
+                  left join pullout_request_details as PORD on POR.RCPNo = PORD.RCPNo 
+                  where PNNo ='${itm.PNo}' and  checkNo ='${itm.Check_No}' and status ='APPROVED'`;
+
+        const dt = (await prisma.$queryRawUnsafe(qry)) as Array<any>;
+
+        if (dt.length <= 0) {
+          return res.send({
+            message: `PN No. : ${itm.PNo} Check No.: ${itm.Check_No} don't have pullout approval!`,
+            success: false,
+            data: [],
+          });
+        }
+      }
+    }
+
+    const status1 = ["Stored", "Endorsed", "Pulled Out"];
+    const status2 = ["Date_Stored", "Date_Endorsed", "Date_Pulled_Out"];
+
+    for (const itm of tableDataSelected) {
+      const qry = `
+                 UPDATE 
+                pdc  SET 
+                  PDC_Status = '${status1[pdcStatusIndex]}' ,
+                  ${status2[pdcStatusIndex]} = now() 
+                  ${pdcStatusIndex === 2 ? `, PDC_Remarks = '${remarks}'` : ""}
+                WHERE 
+                  PNo = '${itm.PNo}' AND 
+                  IDNo = '${itm.IDNo}' AND 
+                  DATE_FORMAT(Date,'%m/%d/%Y') = '${itm.Date}' AND
+                  Name = '${itm.Name}' AND 
+                  DATE_FORMAT(Check_Date,'%m/%d/%Y') = '${itm.CheckDate}' AND 
+                  Check_No = '${itm.Check_No}' AND 
+                  Check_Amnt = '${itm.Check_Amnt}' AND 
+                  Bank = '${itm.Bank}' AND 
+                  PDC_Status = '${itm.PDC_Status}'
+                `;
+
+      await prisma.$queryRawUnsafe(qry);
+    }
+
+    const dd = [
+      "Stored In Warehouse!",
+      "Endorsed for Deposit!",
+      `Pulled Out As ${remarks}`,
+    ];
+
     res.send({
-      message: "successfully",
+      message: `Successfully ${dd[pdcStatusIndex]}`,
       success: true,
-      data: await prisma.$queryRawUnsafe(
-        StrQry,
-        `%${req.body.search}%`,
-        `%${req.body.search}%`
-      ),
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send({
+      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
+      success: false,
+    });
+  }
+});
+Warehouse.post("/warehouse/load-list", async (req, res) => {
+  try {
+    res.send({
+      message: `Successfully Get Data`,
+      success: true,
+      list: await loadList(req, req.body.RCPNo),
+    });
+  } catch (err: any) {
+    console.log(err.message);
+    res.send({
+      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
+      success: false,
+      data: [],
+    });
+  }
+});
+Warehouse.post("/warehouse/get-pullout-rcpno", async (req, res) => {
+  try {
+    res.send({
+      message: `Successfully Get Data`,
+      success: true,
+      data: await getApprovedRCPNo(req),
     });
   } catch (err: any) {
     console.log(err.message);
